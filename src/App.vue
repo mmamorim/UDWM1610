e1c5306b50726f711ad934a7e97b73f60ac6008e
mmamorim <marc.m.amorim@gmail.com>
2023-10-16 20:31:12 -0300

    versão antes do recreio

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    oi gente 
+    <teste />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import teste from "./components/teste.vue"
+
+export default {
+  components: { teste }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style>
+
 </style>
---
+++ src/components/teste.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        Teste {{ contador }} 
+        <button @click="incrementar">Incrementa</button>
+
+        <input type="text" v-model="caixa_nome"/>
+        <div>
+            o valor do atributo caixa_nome é: {{ caixa_nome }}
+        </div>
+        <button @click="adicionaNome">Adicionar nome</button>
+        <div v-for="p in nomes">
+            {{ p }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            contador: 0,
+            nomes: [],
+            caixa_nome: ''
+        }
+    },
+    methods: {
+        
+        incrementar() {
+            this.contador++
+        },
+
+        adicionaNome() {
+            let temp = this.caixa_nome.toUpperCase()
+            this.nomes.push(temp)
+            this.caixa_nome = ''
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
