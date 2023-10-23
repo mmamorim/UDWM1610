8538aaaa99998c5991b4ed4c25a21d639188cf1b
mmamorim <marc.m.amorim@gmail.com>
2023-10-23 20:37:37 -0300

    versao 2

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        Card
+    <div class="border p-1 m-1 rounded">
+        <Card_title title="Algum título legal" icon="xxx"/>
+        <div class="p-1 m-1">
+            Alguma coisa aqui
+        </div>
+        <div class="p-1 m-1">
+            <button class="btn btn-primary">
+                Clique aqui
+            </button>
+        </div>
     </div>
 </template>
 <script>
+import Card_title from './Card_title.vue';
+
 export default {
-    
+    components: { Card_title }
 }
 </script>
-<style lang="">
+<style>
     
 </style>
---
+++ src/components/Card_title.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 class="p-1 m-1 rounded bg-gray-200">
+        <span class="text-lg border bg-white">
+            {{ icon }}
+        </span>
+        {{ title }}
+    </h1>
+</template>
+<script>
+export default {
+    props: {
+        title: { default: "sem título" }, 
+        icon: {}
+    }
+}
+</script>
+<style lang="">
+    
+</style>
